c7d7cfc1b084daed5252073234daf717674b8401
Lindrit <lindritsulaj2.0@gmail.com>
2024-05-09 18:32:53 +0200

    made the h1 font smaller on mobile

--- sections/hero.tsx
+++ sections/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <section className="hero-bg h-[75vh] flex items-center px-4">
       <div className="max-w-screen-lg mx-auto w-full">
-        <h1 className='text-white text-3xl md:text-4xl md:text-center lg:text-left lg:text-5xl font-bold lg:leading-[65px]'>Professional <span>Accounting</span> <br />  & Tax services</h1>
+        <h1 className='text-white text-[26px] md:text-4xl md:text-center lg:text-left lg:text-5xl font-bold lg:leading-[65px]'>Professional <span>Accounting</span> <br />  & Tax services</h1>
         <p className='text-white/85 lg:text-lg mt-2 md:mx-auto lg:mx-0 md:text-center lg:text-left md:max-w-2xl lg:max-w-3xl'>We help businesses of any size with tax planning assitance, financial reporting, bookkeeping and more.</p>
         <div className="flex justify-start md:justify-center lg:justify-start w-full gap-4 mt-4">
           <Link href="/">
